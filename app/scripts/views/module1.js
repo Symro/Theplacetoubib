9b5a626876ea9430c3441a8dd8eac9e590bdf4af
symro <sylvain.mro@gmail.com>
2014-05-13 20:15:30 +0200

    initial commit

---
+++ app/scripts/views/module1.js
@@ -0,0 +1,33 @@
+/*global define*/
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates'
+], function ($, _, Backbone, JST) {
+    'use strict';
+
+    var Module1View = Backbone.View.extend({
+        template: JST['app/scripts/templates/module1.ejs'],
+
+        tagName: 'div',
+
+        id: '',
+
+        className: '',
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+
+        }
+    });
+
+    return Module1View;
+});
