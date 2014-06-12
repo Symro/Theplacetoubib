c5cfc3848d714dce70ce79ad97e4f8ce84db9f29
Alex <alexandre.guerard6@gmail.com>
2014-06-13 00:26:48 +0200

    Twitter + MediaQueries

--- js/app.js
+++ js/app.js
@@ -2043,6 +2043,13 @@ $(document).ready(function() {
     });
 
 
+    $('a.tweet').click(function(e){
+      e.preventDefault();
+      var loc = $(this).attr('href');
+      var title  = escape($(this).attr('title'));
+      window.open('http://twitter.com/share?url=' + loc + '&text=' + title + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    });
+
     /* ********************************************************
 	/ 	D3 MAP
 	/ ********************************************************* */
